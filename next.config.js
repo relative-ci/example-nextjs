ebebc36afde51d936f04f06c5a92d125c31cfb6c
Viorel Cojocaru <vio@beanon.com>
2024-06-06 00:24:06 +0200

    Add relative-ci/agent

--- next.config.js
+++ next.config.js
@@ -1,1 +1,15 @@
-module.exports = {};
+const { RelativeCiAgentWebpackPlugin } = require('@relative-ci/agent');
+
+module.exports = {
+  webpack: function (config, options) {
+    const { dev, isServer } = options;
+
+    if (!dev && !isServer) {
+      config.plugins.push(
+        new RelativeCiAgentWebpackPlugin(),
+      );
+    }
+
+    return config;
+  }
+};
