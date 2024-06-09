5f801355254f386762d9e91d1edf0bdd3d42b248
Viorel Cojocaru <vio@beanon.com>
2024-06-09 19:58:45 +0200

    fix: Transpile esm packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const { RelativeCiAgentWebpackPlugin } = require('@relative-ci/agent');
 
 module.exports = {
+  transpilePackages: ['@ant-design/icons-svg', 'rc-pagination', 'rc-picker', 'rc-util', 'rc-tooltip'],
   webpack: function (config, options) {
     const { dev, isServer } = options;
 
