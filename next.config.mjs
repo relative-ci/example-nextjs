735f69babc9d34a6f1ef3e3a045a00c689fda113
Viorel Cojocaru <vio@beanon.com>
2025-10-23 08:20:20 +0200

    build: Nextjs - build with turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
-import RelativeCIAgentPlugin from '@relative-ci/webpack-plugin';
- 
 /* @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@ant-design/icons-svg', 'rc-pagination', 'rc-picker', 'rc-util', 'rc-tooltip'],
-  webpack: function (config, options) {
-    const { dev, isServer } = options;
-
-    if (!dev && !isServer) {
-      config.plugins.push(
-        new RelativeCIAgentPlugin(),
-      );
-    }
-
-    return config;
-  }
 };
 
 export default nextConfig;
