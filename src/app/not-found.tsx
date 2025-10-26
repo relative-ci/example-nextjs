98c72e532874058070e277144ebad310fdbee75d
Viorel Cojocaru <vio@beanon.com>
2025-10-26 23:11:03 +0100

    refactor: Migrate to app router

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+"use client"
 
 import { PageHeader } from '../layout/page-header';
 import { Main } from '../layout/main';
 import { NotFound } from '../components/not-found';
 
 export default function NotFoundPage() {
-  const router = useRouter();
-
   return (
     <>
       <PageHeader title="Not found" />
       <Main>
-        <NotFound location={router.pathname} />
+        <NotFound />
       </Main>
     </>
   );
---
+++ src/app/repos/page.jsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { PageHeader } from '@/layout/page-header';
+import { Main } from '@/layout/main';
+import { RepoList } from '@/components/repo-list';
+
+export default function ReposPage() {
+  return (
+    <>
+      <PageHeader title="Examples" />
+      <Main>
+        <RepoList />
+      </Main>
+    </>
+  );
+}
